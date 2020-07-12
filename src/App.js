b8a58dd393a2092171c1be52ff2987744826bfb2
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-11 21:15:38 -0600

    :clown: agarciadarce creacion de repo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  useEffect(() =>{
+    document.title = "Le ha dado click " + count + " veces.";
+    return () =>{
+      console.log("desmontando...");
+    }
+  },[count]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. {count}
         </p>
         <a
           className="App-link"
@@ -18,6 +25,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={() => setCount(count+1)}>Aqui :D</button>
       </header>
     </div>
   );
