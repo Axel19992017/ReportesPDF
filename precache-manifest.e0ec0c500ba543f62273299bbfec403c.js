cad6d7ed2be3091da48d27536e3a65382668196c
Axel Eleazar Garcia Darce <agarciadarce@gmail.com>
2020-07-20 22:44:01 -0600

    Updates

---
+++ precache-manifest.e0ec0c500ba543f62273299bbfec403c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "298c33b516ec710784acdf65abf9e2a3",
+    "url": "/ReportesPDF/index.html"
+  },
+  {
+    "revision": "68b44ca1914cd235454e",
+    "url": "/ReportesPDF/static/css/main.92087704.chunk.css"
+  },
+  {
+    "revision": "5ac249363beb9a611795",
+    "url": "/ReportesPDF/static/js/2.7cf20642.chunk.js"
+  },
+  {
+    "revision": "d0f416c0b71d0ec4a4b3180b59613874",
+    "url": "/ReportesPDF/static/js/2.7cf20642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68b44ca1914cd235454e",
+    "url": "/ReportesPDF/static/js/main.e1f7c892.chunk.js"
+  },
+  {
+    "revision": "bdd855db6e39667d78f9",
+    "url": "/ReportesPDF/static/js/runtime-main.b1ff79c2.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/ReportesPDF/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "9e06bf8e4155ad3a942a9ff38f59fbc4",
+    "url": "/ReportesPDF/static/media/RobotoMono-VariableFont_wght.9e06bf8e.ttf"
+  }
+]);
